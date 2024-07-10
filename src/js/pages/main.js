e442f4d5f485d692fa52f8af0ebf0a87322e6038
Gustavo Dias <gustavo7dias@gmail.com>
2024-07-10 10:10:27 -0300

    Setup modal scripts in vue

--- src/js/pages/main.js
+++ src/js/pages/main.js
@@ -2,11 +2,30 @@ import * as vue from "../vendor/vue";
 const { createApp } = vue;
 
 const app = createApp({
-  data() {
-    return {
-      count: 0,
-    };
-  },
+    delimiters: ["[[", "]]"],
+    data() {
+        return { 
+            modal: {
+                menu: false,
+                logout: false
+            } 
+        }
+    },
+    methods: {
+        openModal(name = "") {
+            this.modal[name] = true
+        },
+        closeModal(name = "") {
+            this.modal[name] = false
+        },
+        openLogout() {
+            this.closeModal("menu")
+            this.modal.logout = true
+        },
+        closeLogout() {
+            this.modal.logout = false
+        }
+    },
 });
 
 app.mount("#app");
