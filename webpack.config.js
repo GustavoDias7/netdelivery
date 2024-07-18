3a88b40684ab11adc76bcaed74fe602d7784bce6
Gustavo Dias <gustavo7dias@gmail.com>
2024-07-17 22:12:02 -0300

    Update PurgeCSS to get all paths from templates

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const globAll = require("glob-all");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
 const BundleTracker = require("webpack-bundle-tracker");
-const { extractExtendsPaths, extractIncludePaths } = require("./utils");
+const { getPaths } = require("./utils/recursive-path.js");
 
 module.exports = {
   watch: true,
@@ -23,8 +23,8 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./staticfiles/js/pages/"),
     // publicPath: "auto",
-    publicPath: '/',
-    assetModuleFilename: 'staticfiles/img/[name][ext]',
+    publicPath: "/",
+    assetModuleFilename: "staticfiles/img/[name][ext]",
     filename: "[name].js",
   },
 
@@ -57,23 +57,8 @@ module.exports = {
     }),
 
     ...globAll.sync("./src/templates/pages/**.html").map((src) => {
-      // access the content of extends and includes associated to the pages
-      const data = fs.readFileSync(src);
-      const dirPath = path.parse(src).dir;
-      const extendsPathFromPage = extractExtendsPaths("" + data)[0];
-      const includesPathsFromPage = extractIncludePaths("" + data).map((p) => {
-        return path.join(dirPath, p);
-      });
-      const basePathFromPage = path.join("./", dirPath, extendsPathFromPage);
-      const data2 = fs.readFileSync(basePathFromPage);
-      const includesPaths = extractIncludePaths("" + data2).map((p) => {
-        return path.join(dirPath, "..", p);
-      });
-      includesPaths.push(basePathFromPage);
-      includesPaths.push(src);
-
       return new PurgeCSSPlugin({
-        paths: globAll.sync([...includesPaths, ...includesPathsFromPage], { nodir: true }),
+        paths: globAll.sync(getPaths(src), { nodir: true }),
         only: [path.parse(src).name],
         fontFace: true,
         variables: true,
