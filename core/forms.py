9dbbd7c5eccc7799e75f5b2d7d5aec4abdc6cc95
Gustavo Dias <gustavo7dias@gmail.com>
2024-07-29 18:00:13 -0300

    Create FirstNameForm, LastNameForm, PhoneForm and UsernameForm forms

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import User
+from django.core import validators
+import re
+from django.utils.translation import gettext_lazy as _
 
 class UserForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput())
@@ -12,3 +15,19 @@ class UserForm(forms.ModelForm):
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
+    
+class FirstNameForm(forms.Form):
+    first_name = forms.CharField(max_length=30)
+        
+class LastNameForm(forms.Form):
+    last_name = forms.CharField(max_length=30)
+    
+class PhoneForm(forms.Form):
+    phone = forms.CharField(max_length=11)
+    
+class UsernameForm(forms.Form):
+    phone = forms.CharField(max_length=15, validators=[
+        validators.RegexValidator(
+            re.compile("^[a-zA-Z0-9_.-]+$"), _("Enter a valid username."), _("invalid")
+        )
+    ],)
