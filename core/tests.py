598783423e54b8abf35461a9dffa40cabcf0fef3
Gustavo Dias <gustavo7dias@gmail.com>
2024-08-05 16:33:45 -0300

    Create name validator and the test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from .validators import (name_validator)
 
 # Create your tests here.
+class ValidatorsTestCase(TestCase):
+    def test_name_validator(self):
+        less_then_three_letters = "ab"
+        alphanumeric = "Viniccius13"
+        with_space = "c c"
+        
+        self.assertRaises(ValidationError, name_validator, value=less_then_three_letters)
+        self.assertRaises(ValidationError, name_validator, value=alphanumeric)
+        self.assertRaises(ValidationError, name_validator, value=with_space)
--- core/validators.py
+++ core/validators.py
@@ -1,4 +1,17 @@
-from django.core.validators import (RegexValidator)
+from django.core.validators import RegexValidator
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 numeric_validator = RegexValidator(r'^\d+$', _('Only digit characters.'))
+    
+def name_validator(value: str):
+    if not value.isalpha():
+        raise ValidationError(
+            _("Must contain only letters."),
+            params={"value": value},
+        )
+    elif len(value) < 3:
+        raise ValidationError(
+            _("Minimum 3 letters"),
+            params={"value": value},
+        )
