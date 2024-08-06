daf9d5eedceaf44e50676e1f1ab9dba518f7803c
Gustavo Dias <gustavo7dias@gmail.com>
2024-08-05 23:46:56 -0300

    Create phone validator

--- core/validators.py
+++ core/validators.py
@@ -1,6 +1,7 @@
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
+import re
 
 numeric_validator = RegexValidator(r'^\d+$', _('Only digit characters.'))
     
@@ -15,3 +16,21 @@ def name_validator(value: str):
             _("Minimum 3 letters"),
             params={"value": value},
         )
+        
+def phone_validator(value: str):
+    if not value.isnumeric():
+        raise ValidationError(
+            _("Only digit characters"),
+            params={"value": value},
+        )
+    elif len(value) != 11:
+        raise ValidationError(
+            _("Must have 11 digits"),
+            params={"value": value},
+        )
+    elif value[2] != "9":
+        raise ValidationError(
+            _("The third digit must be 9"),
+            params={"value": value},
+        )
+        
