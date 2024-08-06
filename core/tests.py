15750a688607b19ff2b7f20922c1aa06e1bcd998
Gustavo Dias <gustavo7dias@gmail.com>
2024-08-05 23:47:24 -0300

    Create phone validator test

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from .validators import (name_validator)
+from .validators import (name_validator, phone_validator)
 
 # Create your tests here.
 class ValidatorsTestCase(TestCase):
@@ -12,3 +12,21 @@ class ValidatorsTestCase(TestCase):
         self.assertRaises(ValidationError, name_validator, value=less_then_three_letters)
         self.assertRaises(ValidationError, name_validator, value=alphanumeric)
         self.assertRaises(ValidationError, name_validator, value=with_space)
+        
+    def test_phone_validator(self):
+        telephone_number = ["(21) 4002-8922", "2140028922"]
+        with_letter = ["(21) 90000-000A", "2190000000A"]
+        less_then_eleven_digits = ["(21) 4002", "214002"]
+        eleven_digits_without_nine = ["(21) 00000-0000", "21000000000"]
+        
+        for number in telephone_number:
+            self.assertRaises(ValidationError, phone_validator, value=number)
+            
+        for number in with_letter:
+            self.assertRaises(ValidationError, phone_validator, value=number)
+        
+        for number in less_then_eleven_digits:
+            self.assertRaises(ValidationError, phone_validator, value=number)
+        
+        for number in eleven_digits_without_nine:
+            self.assertRaises(ValidationError, phone_validator, value=number)
