17b2f4cd36cc90cc7c7a86453377d3dba1aa5ff2
Gustavo Dias <gustavo7dias@gmail.com>
2024-08-05 23:49:50 -0300

    Update validations based on user model

--- core/forms.py
+++ core/forms.py
@@ -12,25 +12,30 @@ class UserForm(forms.ModelForm):
         fields = ["first_name", "last_name", "email", "password"]
         
 
-class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput())
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["email", "password"]
     
-class FirstNameForm(forms.Form):
-    first_name = forms.CharField(max_length=30)
+class FirstNameForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["first_name"]
         
-class LastNameForm(forms.Form):
-    last_name = forms.CharField(max_length=30)
+class LastNameForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["last_name"]
     
-class PhoneForm(forms.Form):
-    phone = forms.CharField(max_length=11)
+class PhoneForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["phone"]
     
-class UsernameForm(forms.Form):
-    phone = forms.CharField(max_length=15, validators=[
-        validators.RegexValidator(
-            re.compile("^[a-zA-Z0-9_.-]+$"), _("Enter a valid username."), _("invalid")
-        )
-    ],)
+class UsernameForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["username"]
     
 class CepForm(forms.Form):
     cep = forms.CharField(max_length=8, validators=[validators.MinLengthValidator(8)])
