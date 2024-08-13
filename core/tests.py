409f2facddfd0838f67e33ce58df369a49d65a1d
Gustavo Dias <gustavo7dias@gmail.com>
2024-08-13 14:17:13 -0300

    Create test_first_occurrence

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.exceptions import ValidationError
-from .validators import (name_validator, phone_validator, cep_validator)
+from core.validators import (name_validator, phone_validator, cep_validator)
+from core.utils import first_occurrence
 
 # Create your tests here.
 class ValidatorsTestCase(TestCase):
@@ -42,3 +43,24 @@ class ValidatorsTestCase(TestCase):
         self.assertRaises(ValidationError, cep_validator, value=alphanumeric)
         self.assertRaises(ValidationError, cep_validator, value=less_then_eight_digits)
         self.assertRaises(ValidationError, cep_validator, value=empty_string)
+        
+    def test_first_occurrence(self):
+        uf = "RJ"
+        no_lines = []
+        self.assertEqual(first_occurrence(no_lines, uf), -1)
+        
+        one_line_without_match = ["6787@PR@Tuneiras do Oeste@87450000@0@M@@Tuneiras Oeste@4127908"]
+        one_line_with_match = ["6870@RJ@Bom Jardim@28660000@0@M@@Bom Jardim@3300506"]
+        
+        self.assertEqual(first_occurrence(one_line_without_match, uf), -1)
+        self.assertEqual(first_occurrence(one_line_with_match, uf), 0)
+        
+        two_lines_without_match = ["6788@PR@Tupãssi@85945000@0@M@@Tupãssi@4127957","6789@PR@Tupinambá@86746000@0@D@5798@Tupinambá@" ]
+        two_lines_with_match_0_1_index = ["6873@RJ@Cabo Frio@@1@M@@Cb Frio@3300704","6874@RJ@Cabuçu@@2@D@6941@Cabuçu@"]
+        two_lines_with_match_0_index = ["6874@RJ@Cabuçu@@2@D@6941@Cabuçu@", "7297@RN@Tibau@59678000@0@M@@Tibau@2411056"]
+        two_lines_with_match_1_index = ["6788@PR@Tupãssi@85945000@0@M@@Tupãssi@4127957","6874@RJ@Cabuçu@@2@D@6941@Cabuçu@"]
+        
+        self.assertEqual(first_occurrence(two_lines_without_match, uf), -1)
+        self.assertEqual(first_occurrence(two_lines_with_match_0_1_index, uf), 0)
+        self.assertEqual(first_occurrence(two_lines_with_match_0_index, uf), 0)
+        self.assertEqual(first_occurrence(two_lines_with_match_1_index, uf), 1)
