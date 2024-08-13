f2bd2a457722cf4d1363679df4f4b575fe184bd9
Gustavo Dias <gustavo7dias@gmail.com>
2024-08-13 14:19:15 -0300

    Create Logradouro, UF, Bairro and Localidade models

--- core/models.py
+++ core/models.py
@@ -269,3 +269,53 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # def email_user(self, subject, message, from_email=None):
     #     send_mail(subject, message, from_email, [self.email])
+    
+class Logradouro(models.Model):
+    id = models.PositiveSmallIntegerField(primary_key=True)
+    uf = models.ForeignKey("UF", on_delete=models.RESTRICT)
+    localidade = models.ForeignKey("Localidade", on_delete=models.RESTRICT)
+    bairro = models.ForeignKey("Bairro", on_delete=models.RESTRICT)
+    name = models.CharField(max_length=100)
+    complement = models.CharField(null=True, max_length=100)
+    cep = models.CharField(max_length=8, validators=[cep_validator])
+    type = models.CharField(max_length=36)
+    
+    def __str__(self):
+        return f"{self.type} {self.name}"
+    
+class UF(models.Model):
+    acronym = models.CharField(max_length=2, validators=[MinLengthValidator(2)])
+    
+    def __str__(self):
+        return f"{self.acronym}"
+
+class Bairro(models.Model):
+    id = models.PositiveSmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=72)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class Localidade(models.Model):
+    id = models.PositiveSmallIntegerField(primary_key=True)
+    situacaolocalidade = models.ForeignKey("SituacaoLocalidade", on_delete=models.RESTRICT, null=True)
+    tipolocalidade = models.ForeignKey("TipoLocalidade", on_delete=models.RESTRICT)
+    name = models.CharField(max_length=72)
+    cep = models.CharField(max_length=8, validators=[cep_validator])
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+class SituacaoLocalidade(models.Model):
+    definition = models.TextField(max_length=60)
+    
+    def __str__(self):
+        return f"Situação {self.id}"
+    
+class TipoLocalidade(models.Model):
+    code = models.CharField(max_length=1)
+    definition = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return f"{self.code} - {self.definition}"
+    
