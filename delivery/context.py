567504965a1cbea9d066451f59ba2196dbbdfc95
Gustavo Dias <gustavo7dias@gmail.com>
2024-11-03 21:55:41 -0300

    Add filesystem cache to the context queries

--- delivery/context.py
+++ delivery/context.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+from django.core.exceptions import ObjectDoesNotExist
 from apps.user.models import (
     Contacts, 
     OpeningHours
@@ -13,23 +15,47 @@ def header_categories(request):
         "header_categories": [],
         "header_combos": False
     }
-    qs_categories = Category.objects.all()
     
-    for category in qs_categories:
-        variant = ProductVariant.objects.filter(archived=False, product__category=category).first()
-        if variant: context["header_categories"].append(variant.product.category)
+    categories_cache = cache.get("header_categories")
+    
+    if not categories_cache:
+        try:
+            qs_categories = Category.objects.all()
+            for category in qs_categories:
+                variant = ProductVariant.objects.filter(archived=False, product__category=category).first()
+                if variant: context["header_categories"].append(variant.product.category)
+                
+            qs_combos = Combo.objects.filter(archived=False).first()
+            if qs_combos: context["header_combos"] = True
+            
+            cache.set("header_categories", context)
+        except ObjectDoesNotExist:
+            context["qs_categories"] = None
+            context["header_categories"] = None
+    else:
+        context = categories_cache
         
-    qs_combos = Combo.objects.filter(archived=False).first()
-    if qs_combos: context["header_combos"] = True
-    
     return context
 
 def contacts(request):
     context = {}
     
-    qs_contacts = Contacts.objects.filter().first()
-    qs_opening_hours = OpeningHours.objects.filter(contacts=qs_contacts)
-    context["contacts"] = qs_contacts
-    context["opening_hours"] = qs_opening_hours
+    contacts_cache = cache.get("contacts")
+    if not contacts_cache:
+        try:
+            qs_contacts = Contacts.objects.filter().first()
+            context["contacts"] = qs_contacts
+        except ObjectDoesNotExist:
+            context["contacts"] = None
+        
+        try:
+            qs_opening_hours = OpeningHours.objects.filter(contacts=qs_contacts)
+            context["opening_hours"] = qs_opening_hours
+        except ObjectDoesNotExist:
+            context["opening_hours"] = None
+        
+        cache.set("contacts", context)
+    else:
+        context = contacts_cache
     
     return context
--- delivery/settings.py
+++ delivery/settings.py
@@ -137,3 +137,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": BASE_DIR / "cache",
+        "TIMEOUT": 60,
+        "OPTIONS": {"MAX_ENTRIES": 300},
+    }
+}
