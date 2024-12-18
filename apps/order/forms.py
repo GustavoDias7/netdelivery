3ecb7e0ac37360b3ebebf88289f1c7ca320383b5
Gustavo Dias <gustavo7dias@gmail.com>
2024-12-18 19:01:10 -0300

    Create OrderForm and add custom js

--- apps/order/forms.py
+++ apps/order/forms.py
@@ -1,9 +1,17 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from .models import ShippingFee
+from .models import ShippingFee, Order
         
 class ShippingFeeForm(forms.ModelForm):
     class Meta:
         model = ShippingFee
         fields = "__all__"
         widgets = {'user': forms.HiddenInput()}
+        
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = "__all__"
+        
+    class Media:
+        js = ('js/pages/admin_order.js',)
