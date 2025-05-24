caa7a60ae5210e228ab8d5c81c39b0bcbceff3a9
Gustavo Dias <gustavo7dias@gmail.com>
2025-05-24 10:42:33 -0300

    Remove hide and show features from product model and update currency mask to be imported

--- src/js/pages/admin_order.js
+++ src/js/pages/admin_order.js
@@ -1,9 +1,21 @@
+import IMask from "imask";
+import { currency_mask } from "../utils/constants";
+
 window.addEventListener("load", function () {
   (function ($) {
     $(".status").each(function (index) {
       $(this).on("click", function (event) {
         $("[name='status']").val(event.target.id)
       })
+    });
+
+    const id = [];
+
+    document.querySelectorAll("[data-mask=currency]").forEach((input) => {
+      if (!input.id.includes("__prefix__")) {
+        IMask(input, { mask: currency_mask });
+        id.push(input.id);
+      }
     });
   })(django.jQuery);
 });
