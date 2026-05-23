189e5156521f5e0adace4b41ff4d6008298957a0
Gustavo Dias <gustavo7dias@gmail.com>
2026-05-23 13:55:44 -0300

    Update environment variables

--- delivery/settings.py
+++ delivery/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+!jgjt#elsl-go6!9ui^$k@=clf1mwkq=@so_lwi*&7=x-0+i('
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
